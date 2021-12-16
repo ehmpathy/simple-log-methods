9b5f87dc55b1c6de18e5bc9ec907f68a905e5bfa
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2021-12-16 12:49:57 -0700

    fix(lambda): ensure that lambda log messages are stringified for ideal cloudwatch parsing (#18)

---
+++ src/formatLogContentsForEnvironment.test.ts
@@ -0,0 +1,24 @@
+import { LogLevel } from '.';
+import { SupportedEnvironment } from './constants';
+import { formatLogContentsForEnvironment } from './formatLogContentsForEnvironment';
+import { identifyEnvironment } from './identifyEnvironment';
+
+jest.mock('./identifyEnvironment');
+const identifyEnvironmentMock = identifyEnvironment as jest.Mock;
+
+describe('formatMetadataForEnvironment', () => {
+  it('should not stringify the contents - but should stringify the metadata - if in the local environment', () => {
+    identifyEnvironmentMock.mockReturnValue(SupportedEnvironment.LOCAL);
+    const metadata = { name: 'bob', likes: ['oranges', 'apples'] };
+    const logContents = { level: LogLevel.INFO, timestamp: new Date().toISOString(), message: 'hello world!', metadata };
+    const formatted = formatLogContentsForEnvironment(logContents);
+    expect(formatted).toEqual({ ...logContents, metadata: JSON.stringify(metadata) });
+  });
+  it('should stringify the contents in the aws lambda environment', () => {
+    identifyEnvironmentMock.mockReturnValue(SupportedEnvironment.AWS_LAMBDA);
+    const metadata = { name: 'bob', likes: ['oranges', 'apples'] };
+    const logContents = { level: LogLevel.INFO, timestamp: new Date().toISOString(), message: 'hello world!', metadata };
+    const formatted = formatLogContentsForEnvironment(logContents);
+    expect(formatted).toEqual(JSON.stringify(logContents));
+  });
+});
---
+++ src/formatLogContentsForEnvironment.ts
@@ -0,0 +1,39 @@
+import { LogLevel, SupportedEnvironment } from './constants';
+import { identifyEnvironment } from './identifyEnvironment';
+
+export const formatLogContentsForEnvironment = ({
+  level,
+  timestamp,
+  message,
+  metadata,
+}: {
+  level: LogLevel;
+  timestamp: string;
+  message: string;
+  metadata?: Record<string, any>;
+}) => {
+  const env = identifyEnvironment();
+
+  // if its a "local" environment, then dont stringify the contents - but stringify the metadata to cut down on visual noise
+  if (env === SupportedEnvironment.LOCAL) {
+    return {
+      level,
+      timestamp,
+      message,
+      metadata: JSON.stringify(metadata), // json stringify it to cut down on the visual noise in the console
+    };
+  }
+
+  // if its an aws-lambda environment, then stringify the contents - without stringifying the metadata - to make sure log is fully readable and parseable by cloudwatch
+  if (env === SupportedEnvironment.AWS_LAMBDA) {
+    return JSON.stringify({
+      level,
+      timestamp,
+      message,
+      metadata,
+    });
+  }
+
+  // if it was not one of the above, we have not supported this environment yet
+  throw new Error('unsupported environment detected. this should never occur - and is a bug within simple-leveled-log-methods'); // fail fast
+};
