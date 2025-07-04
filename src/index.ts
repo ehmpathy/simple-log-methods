9f8ef696a7abf9a482402d2ee59acf794e84d654
Ulad Kasach <uladkasach@gmail.com>
2025-07-03 20:12:09 -0500

    fix(practs): bump to latest best

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export { withLogTrail } from './logic/withLogTrail';
 export { LogLevel } from './domain/constants';
 export { LogMethod } from './logic/generateLogMethod';
 export { generateLogMethods, LogMethods } from './logic/generateLogMethods';
+export {
+  ContextLogTrail,
+  LogTrail,
+  HasContextLogTrail,
+} from './domain/LogTrail';
+export { withLogTrail } from './logic/withLogTrail';
